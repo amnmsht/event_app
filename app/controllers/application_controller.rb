404597570afbc3e221c0ac565ad38fbf804bcef3
masahito_amano <amano-m@nttpc.co.jp>
2018-02-05 14:59:42 +0000

    ログインシステムを修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,12 +7,9 @@ class ApplicationController < ActionController::Base
   protected
 
     def configure_permitted_parameters
-      # sign_inのときに、usernameも許可する
-      devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
       # sign_upのときに、usernameも許可する
       devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
       #  account_updateのときに、usernameも許可する
       devise_parameter_sanitizer.permit(:account_update, keys: [:name])
     end
   end
-
