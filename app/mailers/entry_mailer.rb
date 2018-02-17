c4c9d02c540e31a6dd3df5eb128e42b06ed427f4
masahito_amano <amano-m@nttpc.co.jp>
2018-02-17 02:35:56 +0000

    メール機能修正

--- app/mailers/entry_mailer.rb
+++ app/mailers/entry_mailer.rb
@@ -3,6 +3,7 @@ class EntryMailer < ApplicationMailer
     def entry_mail(entry)
       @entry = entry
 
- mail to: @entry.user.email, subject: "お申込み確認メール"
+ #mail to: @entry.user.email, subject: "お申込み確認メール"
+  mail to: @entry.event.user.email, subject: "お申込み確認メール"
 end
 end
