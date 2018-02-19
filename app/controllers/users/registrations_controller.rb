6b39cadab5bf7dc1e84ccfa933d1743c63ecd632
masahito_amano <amano-m@nttpc.co.jp>
2018-02-19 10:19:24 +0000

    認証機能修正

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,5 +1,7 @@
 class Users::RegistrationsController < Devise::RegistrationsController
-  def build_resource(hash=nil)
-  self.resource = resource_class.new_with_session(hash || {}, session)
-end
+
+def build_resource(hash={})
+    hash[:uid] = User.create_unique_string
+    super
+  end
 end
